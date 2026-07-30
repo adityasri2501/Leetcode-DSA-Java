cb6631c12d7c480eced5e681a406daf0b547b3ea
Aditya Kumar Srivastava <adityasrivastava250106@gmail.com>
2026-07-30 23:31:00 +0530

    30/07/26

--- Maximum Product Two Elements/Solution_MaxProTwoElement.java
+++ Maximum Product Two Elements/Solution_MaxProTwoElement.java
@@ -1,5 +1,15 @@
 public class Solution_MaxProTwoElement{
     public int maxPro(int []n){
+        // int max = 0, sec_max = 0;
+        // for (int i : n){
+        //     if (i > max){
+        //         sec_max = max;
+        //         max = i;
+        //     } else if (i > sec_max){
+        //         sec_max = i;
+        //     }
+        // }
+        // return (max-1)*(sec_max-1);
         int max = 0, sec_max = 0;
         for (int i : n){
             if (i > max){
