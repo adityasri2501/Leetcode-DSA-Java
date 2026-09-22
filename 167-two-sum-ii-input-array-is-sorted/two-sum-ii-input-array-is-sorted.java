3bd46ce7d67308d6f2ae1dfa729900c6067ab0d0
ADITYA KUMAR SRIVASTAVA <adityasrivastava250106@gmail.com>
2026-09-22 16:40:44 +0530

    Time: 6 ms (11.73%) | Memory: 48.4 MB (60.97%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -6,19 +6,15 @@ class Solution {
         for (int i = 0; i < numbers.length; i++) {
             arr[0] = i + 1;
 
-            int low = i, high = numbers.length - 1, new_tar = target - numbers[i];
+            int low = i + 1, high = numbers.length - 1, new_tar = target - numbers[i];
             boolean found = false;
 
             while (low <= high) {
                 int mid = Math.floorDiv(low + high, 2);
                 if (numbers[mid] == new_tar) {
-                    if (mid + 1 == i + 1) {
-                        low = mid + 1;
-                    } else {
-                        arr[1] = mid + 1;
-                        found = true;
-                        break;
-                    }
+                    arr[1] = mid + 1;
+                    found = true;
+                    break;
                 } else if (numbers[mid] < new_tar) {
                     low = mid + 1;
                 } else {
